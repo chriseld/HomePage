df9278ff11be88bffbc9a1662c647a620c978f38
Jethan16 <ethan.nel.jones@gmail.com>
2020-02-16 16:05:31 -0500

    pulling

--- assets/js/dock-script.js
+++ assets/js/dock-script.js
@@ -4,7 +4,27 @@ var dock = $(".dock")
 button.on("click", animateMenu)
 
 function animateMenu() {
-    button.addClass("button-open")
+    // button.addClass("button-open")
     dock.addClass("dock-open")
+    $(this).toggleClass("button-open")
 }
 
+$(".close").on("click", closeMenu)
+
+function closeMenu() {
+    console.log("clicked");
+    
+    button.toggleClass("button-open")
+    dock.toggleClass("dock-closed")
+    
+    
+}
+
+
+
+// $(document).ready(function() {
+//     var $toggleButton = $('.toggle-button');
+//     $toggleButton.on('click', function() {
+//       $(this).toggleClass('button-open');
+//     });
+//   });
